dbd5e6319b0fa4863b866086fd641d1301e0f63e
Higor <72944799+Higor-SM@users.noreply.github.com>
2022-04-12 20:42:07 -0300

    maps intro
    Co-Authored-By: Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>

--- sr_soybean/src/pages/Home/Home.js
+++ sr_soybean/src/pages/Home/Home.js
@@ -1,18 +1,24 @@
-import { Text, View, StyleSheet } from 'react-native';
-import Fab from '../../components/Fab'
-import Map from '../../components/Map'
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default function Home({}) {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Fab/>
+      <MapView style={styles.map} />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
