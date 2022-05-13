dc4870d762f6193da27481726860f3ccb84faec0
Juliane Freitas <juliane.freitas55@gmail.com>
2022-05-13 17:25:19 -0300

    teste

--- sr_soybean/src/pages/Profile/Profile.js
+++ sr_soybean/src/pages/Profile/Profile.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function SettingsScreen() {
   return (
     <View >
-      <Text>Profile</Text>
+      <Text>Perfil</Text>
     </View>
   );
 }
