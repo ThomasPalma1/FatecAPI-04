53e6ffb94e60e96c80d9349ee3b6687b32adaf2d
Pedro Willian <70727334+pedrowil12@users.noreply.github.com>
2022-06-05 00:18:19 -0300

    Tela de Clima.
    Co-Authored-By: Juliane Freitas <70216694+JulianeFreitass@users.noreply.github.com>
    Co-Authored-By: Higor <72944799+Higor-SM@users.noreply.github.com>

--- sr_soybean/src/pages/Graficos/addMeteorologia.js
+++ sr_soybean/src/pages/Graficos/addMeteorologia.js
@@ -3,27 +3,20 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import {useState, useEffect} from 'react'
 import * as Location from 'expo-location'
-import { EvilIcons } from '@expo/vector-icons' 
+import { EvilIcons } from '@expo/vector-icons'
+import { getWeather, dailyForecast, showWeather} from 'react-native-weather-api';
 
 import InfoCard from './InfoCard'
 import MainCard from "./MainCard"
 
-import getCurrentWeather from './api'
-
-export default function App() {
-
+export default function App({ navigation }) {
+  const [Clima, setClima] = useState([]);
+  const [location, setLocation] = useState(null);
   const axios = require('axios')
   
-  const [currentTemperature, setCurrentTemperature] = useState('31')
-
-  const [locationCoords, setLocationCoords] = useState(null);
-
   const [locationName, setLocationName] = useState('Brasil, Fortaleza')
   
-  const [temperatureMin, setTemperatureMin] = useState('21')
-  const [temperatureMax, setTemperatureMax] = useState('32')
   const [wind, setWind] = useState('7')
-  const [humidity, setHumidity] = useState('68')
 
   const styles = StyleSheet.create({
     container: {
@@ -99,72 +92,97 @@ export default function App() {
     },  
   });
 
-  async function getLocation(){
-    let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied')
-      }else{
-        let location = await Location.getCurrentPositionAsync({})
-        await setLocationCoords(location.coords)
-      }
-  }
-
-  async function setCurrentWeather(){
-    await getLocation()
-    const data = await getCurrentWeather(locationCoords)
-
-    // Vem da api nessa ordem [currentTemperature, temperatureMin, temperatureMax, locationName, wind, humidity]
-    // console.log(data)
-    // setCurrentTemperature(convertKelvinToC(data[0]))
-    // setTemperatureMin(convertKelvinToC(data[1]))
-    // setTemperatureMax(convertKelvinToC(data[2]))
-    setLocationName(data[0])
-    setWind(data[1])
-    // setHumidity(data[5])
-    
-  }
-// console.log(currentTemperature)
-// console.log(temperatureMin)
-// console.log(temperatureMax)
-// console.log(locationName)
-// console.log(wind)
-// console.log(humidity)
-  function convertKelvinToC(kelvin){
-    return parseInt(kelvin - 273)
-  }
-
   useEffect(() => {
-    setCurrentWeather()
-  }, [])
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permissão de acesso ao local negada');
+        return;
+      }
 
+      let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.00922,
+        longitudeDelta: 0.00421
+      })
+      await dailyForecast({
+        key: "b6fd8e9dbbe23332fcf1a56b08dd8acc",
+        lat: location.coords.latitude,
+        lon: location.coords.longitude,
+        unit: "metric",
+        lang: "pt_br"
+      }).then((data) => {
+        const clima = data;
+        //console.log(clima)
+        const max = clima.daily[0].temp.max;
+        const min = clima.daily[0].temp.min;
+        const currentTemp = clima.current.temp;
+        const currentHumidity = clima.current.humidity;
+        const currentDescription = clima.current.weather[0].description;
+        const sensacaoReal = clima.current.feels_like;
+        var maxRound = Math.round(max) + `°C`;
+        var minRound = Math.round(min) + `°C`;
+        var tempAtual = Math.round(currentTemp);
+        var sensacao = Math.round(sensacaoReal) + `°C`;
+        var descriptionMaiuscula =  currentDescription[0].toUpperCase() + currentDescription.substr(1);
+       
+        setClima({
+          maxima: maxRound,
+          minima: minRound,
+          currentTemp: tempAtual,
+          currentHumidity: currentHumidity,
+          currentDescription: descriptionMaiuscula,
+          sensacao: sensacao
+        })
+      })
+
+      await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=40a5f7559101e8312445dfae9a9b0db1`)
+        .then(function (response){
+            const data = response.data 
+            //console.log(data)
+            const locationName = (data.sys.country + ', ' + ' ' + data.name)
+            const wind = data.wind.speed
+            setLocationName(locationName)
+            setWind(wind)
+            
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+    })
+      ();
+  }, []);
+  
   return (
       <View style={styles.container}>
       
-        <TouchableOpacity style={styles.refreshButton} onPress={() => setCurrentWeather()}>
+        <TouchableOpacity style={styles.refreshButton} onPress={() => navigation.navigate('addMeteorologia')}>
           <EvilIcons name="refresh" color={'white'} size={24}/>
         </TouchableOpacity>
 
         <Feather style={{marginTop: 50}} name="sun" size={40} color="orange" />
 
         <View style={styles.temperatureView}>
-          <Text style={styles.temperatureText}>{currentTemperature}</Text>
+          <Text style={styles.temperatureText}>{Clima.currentTemp}</Text>
           <Text style={[styles.temperatureText, {fontSize: 14}]}>°C</Text>
         </View>
         
         <Text style={styles.localizationText}>{locationName}</Text>
 
         <View style={styles.cardsView}>
-          <MainCard title={"Temperatura Máxima"} icon={'morning'} temperature={temperatureMax} backgroundColor={'#CC6E30'} ></MainCard>
-          <MainCard title={"Temperatura Minima"} icon={'night'} temperature={temperatureMin} backgroundColor={'#008081'} ></MainCard>
+          <MainCard title={"Temperatura Máxima"} icon={'morning'} temperature={Clima.maxima} backgroundColor={'#CC6E30'} ></MainCard>
+          <MainCard title={"Temperatura Minima"} icon={'night'} temperature={Clima.minima} backgroundColor={'#008081'} ></MainCard>
         </View>
     
         <View style={styles.info}>
-          <Text style={styles.infoText}>Informações adcionais:</Text>
+          <Text style={styles.infoText}>Informações adicionais:</Text>
           <View style={styles.addtionalInfo}>
             <InfoCard title={'Vento'} variable={wind} ></InfoCard>
-            <InfoCard title={'Umidade'} variable={humidity} ></InfoCard>
-            <InfoCard title={'Sensação'} variable={temperatureMin} ></InfoCard>
-            <InfoCard title={'Chuva'} variable={temperatureMax} ></InfoCard>
+            <InfoCard title={'Umidade'} variable={Clima.currentHumidity + `%`}></InfoCard>
+            <InfoCard title={'Sensação'} variable={Clima.sensacao} ></InfoCard>
+            <InfoCard title={'Clima'} variable={Clima.currentDescription} ></InfoCard>
           </View>
         </View>       
       </View>
